2f06e14b881bba274c6c8c11f304d392e3dcf218
Renan Cesar <cesar.renan@hotmail.com>
2022-10-31 20:27:23 -0300

    Finalizando Header

---
+++ homework1/src/components/header/index.js
@@ -0,0 +1,25 @@
+import './estilo.css';
+
+export default function Header(){
+    return(
+        <>
+            <header>
+                <div className='brand-container'>
+                <h3>Brandname</h3>
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>Product</li>
+                        <li>Pricing</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+                </div>
+                <div className='login-container'>
+                    <p>Login</p>
+                    <button>JOIN US</button>
+                </div>
+            </header>
+        </>
+    );
+}
