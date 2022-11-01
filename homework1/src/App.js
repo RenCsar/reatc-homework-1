65ad0a7bd96f66ebda327ff96dd1749103be5826
Renan Cesar <cesar.renan@hotmail.com>
2022-10-31 21:58:21 -0300

    Finalizando seção estatus

--- homework1/src/App.js
+++ homework1/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header';
 import Intro from './components/intro';
+import Estatus from './components/status';
 
 function App() {
   return (
     <>
       <Header></Header>
       <Intro></Intro>
+      <Estatus></Estatus>
     </>
   );
 }
---
+++ homework1/src/components/status/index.js
@@ -0,0 +1,26 @@
+import './estilo.css'
+
+export default function Estatus(){
+    return(
+        <>
+            <section className='estatus'>
+                <div>
+                    <h1>15K</h1>
+                    <h5>Happy Customers</h5>
+                </div>
+                <div>
+                    <h1>150K</h1>
+                    <h5>Monthly Visitors</h5>
+                </div>
+                <div>
+                    <h1>15</h1>
+                    <h5>Countries  Worldwide</h5>
+                </div>
+                <div>
+                    <h1>100+</h1>
+                    <h5>Top Partners</h5>
+                </div>
+            </section>
+        </>
+    )
+};
